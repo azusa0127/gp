169302c420885a225e162769ac44e6e9f5047e1d
Phoenix Song <PhoenixSong@Outlook.com>
2018-08-05 01:32:27 -0700

    base64 feature test

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"bytes"
 	"context"
+	"encoding/base64"
 	"encoding/json"
 	"flag"
 	"io"
@@ -17,6 +18,43 @@ import (
 
 var jsonpathQuery = flag.String("q", "", "Query in jsonpath")
 var jsonpathEvalFn gval.Evaluable = func(c context.Context, v interface{}) (interface{}, error) { return v, nil }
+
+var bash64EncodeFlag = flag.Bool("base64e", false, "Flag to encode the result string with base64")
+var bash64DecodeFlag = flag.Bool("base64d", false, "Flag to decode the result string with base64")
+
+func base64helper(v interface{}, w io.Writer, encode bool) error {
+	var err error
+	var buf *bytes.Buffer
+	switch v.(type) {
+	case string:
+		buf = bytes.NewBufferString(v.(string))
+	case []byte:
+		buf = bytes.NewBuffer(v.([]byte))
+	default:
+		b, err := json.Marshal(v)
+		if err != nil {
+			return err
+		}
+		buf = bytes.NewBuffer(b)
+	}
+
+	if encode {
+		_, err = io.Copy(base64.NewEncoder(base64.StdEncoding, w), buf)
+	} else {
+		_, err = io.Copy(w, base64.NewDecoder(base64.StdEncoding, buf))
+	}
+	return err
+}
+
+func process(v interface{}) (interface{}, error) {
+	var err error
+	v, err = jsonpathEvalFn(context.Background(), v)
+	if err != nil {
+		return v, err
+	}
+
+	return v, nil
+}
 
 func main() {
 	flag.Parse()
@@ -46,9 +84,8 @@ func main() {
 			log.Fatalln(err)
 		}
 	}
-
-	enc := json.NewEncoder(os.Stdout)
-	enc.SetIndent("", "  ")
+	jsonEnc := json.NewEncoder(os.Stdout)
+	jsonEnc.SetIndent("", "  ")
 
 	scanner := bufio.NewScanner(src)
 	for scanner.Scan() {
@@ -56,11 +93,23 @@ func main() {
 		if err = json.Unmarshal(scanner.Bytes(), &buf); err != nil {
 			log.Fatalln(err)
 		}
+
 		buf, err = jsonpathEvalFn(context.Background(), buf)
 		if err != nil {
 			log.Fatalln(err)
 		}
-		enc.Encode(buf)
+
+		switch {
+		case *bash64EncodeFlag:
+			err = base64helper(buf, os.Stdout, true)
+		case *bash64DecodeFlag:
+			err = base64helper(buf, os.Stdout, false)
+		default:
+			err = jsonEnc.Encode(buf)
+		}
+		if err != nil {
+			log.Fatalln(err)
+		}
 	}
 	if err = scanner.Err(); err != nil {
 		log.Fatalln(err)
