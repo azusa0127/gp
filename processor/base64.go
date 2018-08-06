93d64d2f2523ee021f0444abd3fc2ee0b860cb8b
Phoenix Song <phoenixsong@outlook.com>
2018-08-05 23:49:08 -0700

    base64e

--- processor/base64.go
+++ processor/base64.go
@@ -8,17 +8,15 @@ import (
 
 // Base64encodeProcessor is the Processor implementation for Base64 encoding
 type Base64encodeProcessor struct {
-	s   *bufio.Scanner
-	w   io.Writer
-	enc io.WriteCloser
+	s *bufio.Scanner
+	w io.Writer
 }
 
 // NewBase64encodeProcessor creates and initializes a Base64encodeProcessor
 func NewBase64encodeProcessor(src io.Reader, dst io.Writer) *Base64encodeProcessor {
 	return &Base64encodeProcessor{
-		s:   bufio.NewScanner(src),
-		w:   dst,
-		enc: base64.NewEncoder(base64.StdEncoding, dst),
+		s: bufio.NewScanner(src),
+		w: dst,
 	}
 }
 
@@ -26,17 +24,19 @@ func NewBase64encodeProcessor(src io.Reader, dst io.Writer) *Base64encodeProcess
 func (b *Base64encodeProcessor) Process() error {
 	var err error
 	for b.s.Scan() {
-		_, err = b.enc.Write(b.s.Bytes())
-		if err != nil {
+		enc := base64.NewEncoder(base64.StdEncoding, b.w)
+		if _, err = enc.Write(b.s.Bytes()); err != nil {
 			return err
 		}
-		_, err = b.w.Write(LineBreakBytes)
-		if err != nil {
+		if err = enc.Close(); err != nil {
+			return err
+		}
+		if _, err = b.w.Write(LineBreakBytes); err != nil {
 			return err
 		}
 	}
 	if err = b.s.Err(); err != nil {
 		return err
 	}
-	return b.enc.Close()
+	return nil
 }
