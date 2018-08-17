b5d164e36217dedb0a9417c7026605c55df90363
Phoenix Song <PhoenixSong@Outlook.com>
2018-08-17 11:34:26 -0700

    interface proposed.

---
+++ pipeline/interface.go
@@ -0,0 +1,54 @@
+package pipeline
+
+import "errors"
+
+var (
+	// ErrNilArgV is the error returned when a nil buffer pointer v is passed in to Unmarshal()
+	ErrNilArgV = errors.New("Value of buffer pointer argument v for Unmarshal() cannot be nil")
+
+	// ErrInvalidTypeArgV is the error returned when the value type of argument v is unsupported
+	ErrInvalidTypeArgV = errors.New("Value of argument v for Marshal()/Unmarshal() is passed in a type unsupported")
+)
+
+type Unmarshaller interface {
+	Unmarshal(s []byte, v interface{}) error
+}
+
+type Marshaller interface {
+	Marshal(v interface{}) ([]byte, error)
+}
+
+var (
+	// ErrNullUnmarshallerInvalidTypeArgV is the error returned when the value type of argument v is unsupported
+	ErrNullUnmarshallerInvalidTypeArgV = errors.New("NullUnmarshaller.Unmarshal() accepts buffer pointer v only as a non nil pointer in type *[]byte")
+
+	// ErrNullMarshallerInvalidTypeArgV is the error returned when the value type of argument v is unsupported
+	ErrNullMarshallerInvalidTypeArgV = errors.New("NullMarshaller.Marshal() accepts argument v only in type []byte")
+)
+
+type NullUnmarshaller struct{}
+
+func (n *NullUnmarshaller) Unmarshal(s []byte, v interface{}) error {
+	if v == nil {
+		return ErrNilArgV
+	}
+	ptr, ok := v.(*[]byte)
+	if !ok {
+		return ErrNullUnmarshallerInvalidTypeArgV
+	}
+	*ptr = s
+	return nil
+}
+
+type NullMarshaller struct{}
+
+func (n *NullMarshaller) Marshal(v interface{}) ([]byte, error) {
+	if v == nil {
+		return nil, ErrNilArgV
+	}
+	b, ok := v.([]byte)
+	if !ok {
+		return nil, ErrNullMarshallerInvalidTypeArgV
+	}
+	return b, nil
+}
---
+++ pipeline/interface_test.go
@@ -0,0 +1,77 @@
+package pipeline
+
+import (
+	"bytes"
+	"reflect"
+	"testing"
+)
+
+func TestNullUnmarshaller_Unmarshal(t *testing.T) {
+	tests := []struct {
+		name    string
+		input   []byte
+		want    []byte
+		wantErr bool
+	}{
+		{
+			"Unmarshals valid bytes correctly",
+			[]byte("abcde"),
+			[]byte("abcde"),
+			false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			n := &NullUnmarshaller{}
+			var got []byte
+			if err := n.Unmarshal(tt.input, &got); (err != nil) != tt.wantErr {
+				t.Errorf("NullUnmarshaller.Unmarshal() error = %v, wantErr %v", err, tt.wantErr)
+			}
+
+			if bytes.Compare(got, tt.want) != 0 {
+				t.Errorf("NullUnmarshaller.Unmarshal() result mismatch: Actual = %s, Wanted %s", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestNullMarshaller_Marshal(t *testing.T) {
+	tests := []struct {
+		name    string
+		input   interface{}
+		want    []byte
+		wantErr bool
+	}{
+		{
+			"Marshal() should return the exact input back if in []byte",
+			[]byte("abcde"),
+			[]byte("abcde"),
+			false,
+		},
+		{
+			"Marshal() should return the error if input in type other than []byte",
+			"abcde",
+			nil,
+			true,
+		},
+		{
+			"Marshal() should return nil if input in is nil with no error",
+			nil,
+			nil,
+			false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			n := &NullMarshaller{}
+			got, err := n.Marshal(tt.input)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("NullMarshaller.Marshal() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NullMarshaller.Marshal() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
